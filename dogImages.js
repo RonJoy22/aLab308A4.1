f96eb8efb3c083eddd89765475111240e16d79aa
ronald.joyner-brown <r.brownjoyner@gmail.com>
2024-09-17 14:12:41 -0400

    first commit

---
+++ dogImages.js
@@ -0,0 +1,19 @@
+import { start } from "./api.js";
+import { createBreedList} from "./app.js"
+
+export async function loadByBreed(breed){
+    if(breed != "Choose A Dog Breed"){
+      const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
+      const data = await response.json()
+      createSlideshow(data.message)
+    }
+    
+  }
+  
+export async function createSlideshow(images){
+    document.getElementById("slideshow").innerHTML = `
+    <div class="slide" style="background-image: url('${images[0]}')">
+              </div>`
+  
+  }
+  
